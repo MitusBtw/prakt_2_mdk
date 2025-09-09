4abb3596b73f383cce1885e2c1067b3c9b443cbf
MitusBtw <denpro03@bk.ru>
2025-09-09 22:19:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            int number = 12345;
+
+            System.out.print("Обратное число: ");
+
+
+            while (number > 0) {
+                System.out.print(number % 10);
+                number = number / 10;
+            }
+        }
+    }
